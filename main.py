787123650f7c8aecfeae983039f65fe9c3f60480
fant1es <genri0206@gmail.com>
2026-06-01 00:02:40 +0300

    Update bot's answers and menu

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 import os
 from dotenv import load_dotenv
 
-
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardBuilder
 
 load_dotenv()
 
@@ -12,33 +12,42 @@ load_dotenv()
 bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Нажми меня", callback_data="btn_1")],
-    [InlineKeyboardButton(text="Ссылка", url="https://example.com")]
-])
+def get_menu_keyboard() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Загрузить файл", callback_data="download_file")
+    builder.button(text="Загрузить по ссылке", callback_data="download_by_url")
+    builder.button(text="Справка", callback_data="help")
+    builder.button(text="Об авторах", callback_data="about_authors")
 
-@dp.message(Command("menu"))
-async def show_menu(message: types.Message):
-    await message.answer("Выберите действие:", reply_markup=keyboard)
+    # Разделяет кнопки по числу на строку
+    builder.adjust(2, 1, 1)
 
-@dp.callback_query()
-async def handle_callback(callback: types.CallbackQuery):
-    await callback.answer()  # убирает "часики" на кнопке
-    await callback.message.edit_text(f"Вы нажали: {callback.data}")
+    return builder.as_markup()
 
-# Обработчик команды /start
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет! Я бот на aiogram!")
+    await message.answer("Добро пожаловать в наш конвертер!\nЗдесь вы можете получить аудио из вашего видео.")
+    await cmd_menu(message)
 
-# Обработчик простого текста
+
+@dp.message(Command("menu"))
+async def cmd_menu(message: types.Message):
+    await message.answer("Выберите действие:", reply_markup=get_menu_keyboard())
+
+
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(f"Вы написали: {message.text}")
 
-# Запуск бота
+
+@dp.callback_query()
+async def handle_callback(callback: types.CallbackQuery):
+    await callback.answer()  # Без всплывающего уведомления
+    await callback.message.edit_text(f"Вы нажали: {callback.data}")
+
+
 async def main():
     await dp.start_polling(bot)
     
