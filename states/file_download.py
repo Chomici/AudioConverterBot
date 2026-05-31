db4cb21e7fd29e4375a74e964c92121743e99558
fant1es <genri0206@gmail.com>
2026-06-01 01:43:17 +0300

    Remove waiting_action state from FileDownloadState

--- states/file_download.py
+++ states/file_download.py
@@ -2,7 +2,5 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class FileDownloadState(StatesGroup):
-    waiting_action = State()
     waiting_file = State()
     waiting_file_format = State()
-    
