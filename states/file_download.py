46882e7fe4617a669a0e88e38a93dcb011e1047d
fant1es <genri0206@gmail.com>
2026-06-02 09:23:35 +0300

    Add FSM states for url download flow

--- states/file_download.py
+++ states/file_download.py
@@ -4,3 +4,9 @@ from aiogram.fsm.state import State, StatesGroup
 class FileDownloadState(StatesGroup):
     waiting_file = State()
     waiting_file_format = State()
+
+
+class URLDownloadState(StatesGroup):
+    waiting_url = State()
+    waiting_video_format = State()
+    waiting_audio_format = State()
